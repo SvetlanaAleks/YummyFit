a972ddfac58f80f225bd475b1a5a5155c4f4fbca
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-11 12:52:26 +0200

    add reviews block + adaptive

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -20,8 +20,28 @@ const App = (function () {
             }, ]
          });
       },
+      sliderReviewsInit: function () {
+         $('.reviews__slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: "linear",
+            prevArrow: '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
+            nextArrow: '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
+            autoplay: false,
+            adaptiveHeight: true,
+            responsive: [{
+               breakpoint: 768,
+               settings: {
+                  adaptiveHeight: true
+               }
+            }, ]
+         });
+      },
       init: function () {
          App.sliderResultInit();
+         App.sliderReviewsInit();
       }
    };
 })();
