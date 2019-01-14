dff5bf3eb73300e1e12b73ff205d560e475e7a89
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-14 15:12:08 +0200

    add animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 //=require ../../node_modules/slick-carousel/slick/slick.min.js
 //=require partials/app.js
+$(document).ready(function () {
+   App.init();
+   new WOW().init();
+});
 
 $(function () {
    objectFitImages();
-});
-
-$(document).ready(function () {
-   App.init();
 });
