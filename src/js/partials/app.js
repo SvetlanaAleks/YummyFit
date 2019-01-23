344755fc5372a91cfce185b82eba75f8e8450b14
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-23 10:38:40 +0200

    new pack

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -7,17 +7,34 @@ const App = (function () {
             dots: true,
             infinite: true,
             speed: 500,
-            fade: true,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             cssEase: "linear",
             prevArrow: '<div class="arr arr--left"><i class="fico fico-arrow"></i></div>',
             nextArrow: '<div class="arr arr--right"><i class="fico fico-arrow"></i></div>',
             autoplay: false,
             responsive: [{
-               breakpoint: 768,
-               settings: {
-                  adaptiveHeight: true
+                  breakpoint: 1024,
+                  settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 2,
+                  }
+               },
+               {
+                  breakpoint: 768,
+                  settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 2,
+                  }
+               },
+               {
+                  breakpoint: 640,
+                  settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 1,
+                  }
                }
-            }, ]
+            ]
          });
       },
       sliderReviewsInit: function () {
